fe990632d3e637df69659495c75efee51ec529d3
zxx <1193997809@qq.com>
2021-10-25 22:09:45 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+// 加载vant核心组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
